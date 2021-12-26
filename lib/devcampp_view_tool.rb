b1eead4c6aae52de334f51f381aca68109da34d5
Yamin1234 <yamin213811@example.com>
2021-12-26 15:47:08 +0630

    Dev tool initial commit

---
+++ lib/devcampp_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "devcampp_view_tool/version"
+require_relative "devcampp_view_tool/renderer"
+
+module DevcamppViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
